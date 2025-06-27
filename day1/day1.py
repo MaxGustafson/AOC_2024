cda57dd910f3e7985ca116762bb64386b66c599a
Max Gustafson <maxgustafson97@gmail.com>
2025-06-27 17:14:38 +0200

    Added input folder

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,17 @@
+def parse_input(file_name : str):
+    print(f"file_name {file_name}")
+
+    with open(file_name, "r") as file:
+        for line in file:
+            print("###")
+            print(line.strip())  # .strip() removes the newline character
+    
+
 def main():
     print('Hello World')
 
 if __name__ == '__main__':
     main()
+    input_file = "input_mini.txt"
+    input_file_path = "data/" + input_file
+    parse_input(input_file_path)
