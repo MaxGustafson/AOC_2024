e71f3709343b34a6c2c310c8fc3cfb7f5002a4b1
Max Gustafson <maxgustafson97@gmail.com>
2025-06-28 12:04:44 +0200

    final part2 solution

--- day2/day2.py
+++ day2/day2.py
@@ -19,9 +19,8 @@ def parse_input(file_name : str, debug : int = False):
 
     
 '''
-    Return
-        1 = safe
-        0 = unsafe
+    Return index of unsafe level. return of -1 indicates safe report
+    
 '''
 def evaluate_report(report : list, debug = False):
     if(report is None):
@@ -32,40 +31,66 @@ def evaluate_report(report : list, debug = False):
     delta = 0
     delta_pre = 0
 
-    nbr_unsafe_levels = 0
-
     for i in range(1,len(report),1):
         delta = report[i] - report[i-1]
         if debug:
             print(f"iteration {i}")
             print(f"delta = {delta}")
             print(f"delta_pre = {delta_pre}")
-            print(f"nbr of unsafe levels = {nbr_unsafe_levels}")
 
         if abs(delta) > 3 or\
               delta == 0 or\
                   (i != 1 and np.sign(delta) != np.sign(delta_pre)):
-             nbr_unsafe_levels += 1
-             print(f"level unsafe. nbr of unsafe levels = {nbr_unsafe_levels}")
+             return i
              
-        if nbr_unsafe_levels > 1:
-            return 0
         delta_pre = delta
 
-    return 1
+    return -1
 
     
+def evaluate_report_with_buffer(report: list, debug : bool = False):
+    if debug:
+        print(report)
+
+    #Begin by evaluating full report
+    i = evaluate_report(report)
+
+    #If the first report is a success, return
+    if i == -1 :
+        if debug:
+            print("Report safe")
+        return 1
+
+    for j in range(len(report)):
+        buffered_report = report.copy()
+        buffered_report.pop(j)
+
+        if debug:
+            print(f"Modified report {buffered_report}")
+
+        ii = evaluate_report(buffered_report)
+
+        if ii == -1:
+            if debug:
+                print("Modified report safe")
+            return 1
+        
+    if debug:
+        print("Report unsafe")
+    return 0
      
 def main(debug = False):
-    input_file = "input_mini.txt"
+    input_file = "input.txt"
     input_file_path = "day2/data/" + input_file
     report_list = parse_input(input_file_path)
 
     nbr_safe : int = 0
 
+    #nbr_safe += evaluate_report_with_buffer(report_list[4])
+    
     for i in range(len(report_list)):
-        nbr_safe+=evaluate_report(report_list[i], debug)
-
+        nbr_safe+=evaluate_report_with_buffer(report_list[i], debug)
+    
     print(f"\nNumber safe report = {nbr_safe}")
    
 if __name__ == '__main__':
